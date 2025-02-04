abede11d9bccc5aa2b5bc2eae93d8ff44de83722
monson-steven-0 <Monsonsteven767@gmail.com>
2025-02-03 18:21:19 -0600

    Create TaskRepository.kt

---
+++ TaskRepository.kt
@@ -0,0 +1,16 @@
+// TaskRepository.kt
+class TaskRepository(private val taskDao: TaskDao) {
+    val allTasks: Flow<List<Task>> = taskDao.getAllTasks()
+
+    suspend fun insert(task: Task) {
+        taskDao.insert(task)
+    }
+
+    suspend fun update(task: Task) {
+        taskDao.update(task)
+    }
+
+    suspend fun delete(task: Task) {
+        taskDao.delete(task)
+    }
+}
