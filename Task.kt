20b55b8ac0265b99fd52eb4cbbe1c38b79e3fe68
monson-steven-0 <Monsonsteven767@gmail.com>
2025-02-03 18:20:01 -0600

    Create Task.kt

---
+++ Task.kt
@@ -0,0 +1,9 @@
+// Task.kt
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "is_completed") val isCompleted: Boolean = false,
+    @ColumnInfo(name = "created_at") val createdAt: Long = System.currentTimeMillis()
+)
